0fdbe6b0e5158bb4b10742f9b7e318e593dd4400
gwjeondev <gw4609@gmail.com>
2022-07-11 13:13:48 +0900

    [타입스크립트 소개와 배경] 자바스크립트를 타입스크립트 처럼 코딩하는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,3 +1,17 @@
-const sum = (a, b) => {
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// js를 ts처럼 활용하는 방법
+// @ts-check
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns {number}
+ */
+const add2 = (a, b) => {
   return a + b;
 };
+
+//error message 발생
+add(10, '20');
