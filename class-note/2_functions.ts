e98e8d3b829041e31ea45cd3641d2bb9cc58284e
gwjeondev <gw4609@gmail.com>
2022-07-11 14:50:21 +0900

    [타입스크립트 기초 - 변수와 함수 타입 정의하기] 함수 타입 - 파라미터를 제한하는 특성

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -1,3 +1,5 @@
 //parameter type a, b number
 //return value type number
 const sum = (a: number, b: number): number => a + b;
+
+//별개로, js에서는 함수의 parameter가 2개가 정의 되어있지만, 호출할 때 arguments를 2개 이상 넣어도 아무 문제가 없지만, ts에서는 이를 catch하여 알려줌.
