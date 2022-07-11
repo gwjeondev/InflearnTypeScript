6c87d0302ca3d6bf7804154acecf1c3522c9cc15
gwjeondev <gw4609@gmail.com>
2022-07-11 11:13:54 +0900

    [타입스크립트 소개와 배경] 자바스크립에 타입이 있을 때의 첫번째 장점

---
+++ why-ts/app.js
@@ -0,0 +1,44 @@
+// api url
+const url = 'https://jsonplaceholder.typicode.com/users/1';
+
+// dom
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const address = document.querySelector('#address');
+
+// user data
+const user = {};
+
+/**
+ * @typedef {object} Address 
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+const fetchUser = () => {
+  return axios.get(url);
+}
+
+const startApp = () => {
+  fetchUser()
+    .then(function (response) {
+      console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
