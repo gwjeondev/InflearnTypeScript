cce4abb1708e157c9d02ff4d65197ffd5ba786e9
gwjeondev <gw4609@gmail.com>
2022-07-11 16:25:23 +0900

    [첫번째 프로젝트 - 할일 관리 애플리케이션] 실습 방법 안내 및 any, void 소개

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,52 +1,63 @@
-let todoItems;
+class TodoItem {
+  id: number;
+  title: string;
+  done: boolean;
 
-// api
-function fetchTodoItems() {
-  const todos = [
-    { id: 1, title: '안녕', done: false },
-    { id: 2, title: '타입', done: false },
-    { id: 3, title: '스크립트', done: false },
-  ];
-  return todos;
+  constructor(id: number, title: string, done: boolean) {
+    this.id = id;
+    this.title = title;
+    this.done = done;
+  }
 }
+
+let todoItems: Array<TodoItem>;
+
+const fetchTodoItems = (): Array<TodoItem> => {
+  const todos: Array<TodoItem> = [];
+  todos.push(new TodoItem(1, '안녕', false));
+  todos.push(new TodoItem(2, '타입', false));
+  todos.push(new TodoItem(3, '스크립트', false));
+
+  return todos;
+};
 
 // crud methods
-function fetchTodos() {
-  const todos = fetchTodoItems();
+const fetchTodos = (): Array<TodoItem> => {
+  const todos: Array<TodoItem> = fetchTodoItems();
   return todos;
-}
+};
 
-function addTodo(todo) {
+const addTodo = (todo: TodoItem): void => {
   todoItems.push(todo);
-}
+};
 
-function deleteTodo(index) {
+const deleteTodo = (index: number): void => {
   todoItems.splice(index, 1);
-}
+};
 
-function completeTodo(index, todo) {
+const completeTodo = (index: number, todo: TodoItem): void => {
   todo.done = true;
   todoItems.splice(index, 1, todo);
-}
+};
 
 // business logic
-function logFirstTodo() {
+const logFirstTodo = (): TodoItem => {
   return todoItems[0];
-}
+};
 
-function showCompleted() {
-  return todoItems.filter(item => item.done);
-}
+const showCompleted = (): Array<TodoItem> => {
+  return todoItems.filter((item: TodoItem) => item.done);
+};
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
-function addTwoTodoItems() {
+const addTwoTodoItems = (): void => {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
-}
+};
 
 // NOTE: 유틸 함수
-function log() {
+const log = (): void => {
   console.log(todoItems);
-}
+};
 
 todoItems = fetchTodoItems();
 addTwoTodoItems();
