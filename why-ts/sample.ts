51ddec054636d8b469434e506fa89cfbeeaeb014
gwjeondev <gw4609@gmail.com>
2022-07-11 11:50:21 +0900

    [타입스크립트 소개와 배경] 자바스크립트에 타입이 있을 때의 두 번째 장점

--- why-ts/sample.ts
+++ why-ts/sample.ts
@@ -5,3 +5,4 @@ const add = (a: number, b: number): number => {
 const num = add(10, 20);
 
 // add()의 return 타입이 number로 지정이 되어있기 때문에, num 변수를 호출하여 프로퍼티나, 메서드를 사용하고자 할 때 number 타입에 대한 것을 보여준다.
+//add.
