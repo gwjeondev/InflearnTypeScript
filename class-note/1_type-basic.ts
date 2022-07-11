86733d27508ed580e01cb4335598a853c22a985c
gwjeondev <gw4609@gmail.com>
2022-07-11 14:38:31 +0900

    [타입스크립트 기초 - 변수와 함수 타입 정의하기] 기본 타입 - 문자열, 숫자, 배열

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,16 +1,26 @@
 //number
-//const num = 10;
 const num: number = 10;
 
 //string
-//const str = 'hello';
 const str: string = 'hello';
 
-//Array<number>
-//const numAry = [1,2,3];
+//Array - number
 const numAry: Array<number> = [1, 2, 3];
 
-//Array<string>
-//const strAry = ['jeon', 'park'];
+//Array - string
 const strAry: Array<string> = ['jeon', 'park'];
-const strAry2: string[] = ['jeon', 'park'];
+const strAry2: string[] = ['jeon', 'park']; //strAry === strAry2
+
+//Tuple - Array in value several types
+const address: [string, number] = ['gangnam', 99999];
+
+//object
+const obj: object = {};
+//object property type defined
+const person: {name: string, age: number} = {
+  name: 'jeon',
+  age: 29
+}
+
+//boolean
+const show: boolean = true;
