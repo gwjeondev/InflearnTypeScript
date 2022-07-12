4d0ccb94e821b7808f24db0796bfc4c941f37951
gwjeondev <gw4609@gmail.com>
2022-07-12 15:06:08 +0900

    [이넘] 이넘 활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -21,3 +21,31 @@ console.log(StringEnumShoes.Nike); //나이키
 console.log(StringEnumShoes.Adidas); //아디다스
 console.log(StringEnumShoes.NewBalance); //뉴발란스
 console.log(StringEnumShoes.Fila); //필라
+
+//Enum 활용 사례(드롭 다운 사례나, 미리 정의된 값을 사용 활용할 때)
+// const askQuestion = (answer) => {
+//   if(answer === 'yes') {
+//     console.log('정답');
+//   }
+//   else if(answer === 'no') {
+//     console.log('오답');
+//   }
+// }
+// //불 확실한 값 전달 가능성 있음
+// askQuestion('Yes');
+// askQuestion('N');
+enum Answer {
+  Yes = 'Y',
+  No = 'N'
+}
+
+const askQuestion = (answer: Answer) => {
+    if (answer === Answer.Yes) {
+      console.log('정답');
+    } else if (answer === Answer.No) {
+      console.log('오답');
+    }
+}
+//명확한 값 전달 가능
+askQuestion(Answer.Yes);
+askQuestion(Answer.No);
