699604baaee7792e0c09f92c91999705337b3e21
gwjeondev <gw4609@gmail.com>
2022-07-12 14:53:54 +0900

    [이넘] 문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -9,3 +9,15 @@ console.log(NumberEnumShoes.Nike); //0
 console.log(NumberEnumShoes.Adidas); //1
 console.log(NumberEnumShoes.NewBalance); //10
 console.log(NumberEnumShoes.Fila); //1
+
+//문자형 Enum
+enum StringEnumShoes {
+  Nike = '나이키',
+  Adidas = '아디다스',
+  NewBalance = '뉴발란스',
+  Fila = '필라'
+}
+console.log(StringEnumShoes.Nike);
+console.log(StringEnumShoes.Adidas);
+console.log(StringEnumShoes.NewBalance);
+console.log(StringEnumShoes.Fila);
