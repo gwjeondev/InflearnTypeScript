03586de7ecbe2a973de568ab5dc45c88dbb6dda9
gwjeondev <gw4609@gmail.com>
2022-07-23 16:16:55 +0900

    [타입추론] 복잡한 구조에서의 타입추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -12,3 +12,19 @@ const shoppingItem: Dropdown<string> = {
   value: 'shirt',
   title: 'clothes'
 };
+
+//타입 추론 기본3
+interface Person<T> {
+  name: T;
+  age: number;
+}
+//Person 인터페이스를 상속받는 Woman은, 제네릭 타입도 Person에게 전달됨.
+interface Woman<T> extends Person<T>{
+  job: T;
+}
+//eunbyul 객체의 타입은 Woman이며, 제네릭 타입은 string이고 해당 Woman 인터페이스는 Person 인터페이스를 상속하므로 제네릭으로 string이 전달됨.
+const eunbyul:Woman<string> = {
+  name: 'eunbyul',
+  age: 25,
+  job: 'developer'
+}
